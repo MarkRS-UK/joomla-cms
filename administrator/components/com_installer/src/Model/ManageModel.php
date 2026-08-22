d2f0cd07fcef8fc4ba50208a86ddbd21be72e92b
Mark <mark@goodunlimited.co.uk>
2026-08-22 11:37:22 +0100

    Update from upstream

--- administrator/components/com_installer/src/Model/ManageModel.php
+++ administrator/components/com_installer/src/Model/ManageModel.php
@@ -442,23 +442,26 @@ class ManageModel extends InstallerModel
         $changelog->setVersion($source === 'manage' ? $extension->version : $extension->updateVersion);
         $changelog->loadFromXml($changelogurl);
 
-        // Read all the entries
-        $entries = [
-            'security' => [],
-            'fix'      => [],
-            'addition' => [],
-            'change'   => [],
-            'remove'   => [],
-            'language' => [],
-            'note'     => [],
-        ];
+			// Read all the entries
+			$entries = [
+				'security' => [],
+				'fix'      => [],
+				'addition' => [],
+				'change'   => [],
+				'remove'   => [],
+				'language' => [],
+				'note'     => [],
+			];
 
-        foreach (array_keys($entries) as $name) {
-            $field = $changelog->get($name);
-            if ($field) {
-                $entries[$name] = $changelog->get($name)->data;
-            }
-        }
+			$items = [];
+			foreach (array_keys($entries) as $name) {
+				if (property_exists($entry, $name)) {
+					$items[$name] = $entry->$name->data;
+				}
+			}
+			if (\count($items)) {
+				$modified[$key] = $items;
+			}
 
         $layout = new FileLayout('joomla.installer.changelog');
         $output = $layout->render($entries);
